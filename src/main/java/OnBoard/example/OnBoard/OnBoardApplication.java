076d1cd7907dee25aa8a95e11231a0570ee05917
Emam96 <emamsh990@gmail.com>
2021-11-03 17:48:51 +0200

    starter

---
+++ src/main/java/OnBoard/example/OnBoard/OnBoardApplication.java
@@ -0,0 +1,13 @@
+package OnBoard.example.OnBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OnBoard/example/OnBoard/OnBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package OnBoard.example.OnBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
