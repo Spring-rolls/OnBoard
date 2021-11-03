f9862c2be3c8ef545f0391112055c1f3996f4df7
Emam96 <emamsh990@gmail.com>
2021-11-03 18:21:08 +0200

    added the webconfig

---
+++ src/main/java/OnBoard/example/OnBoard/security/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package OnBoard.example.OnBoard.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig   extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.cors().disable().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login*").permitAll()
+                .antMatchers("/signup*").permitAll()
+                .antMatchers("/index*").permitAll()
+                .antMatchers("/style.css").permitAll()
+                .antMatchers("/styles/style.css").permitAll()
+                .antMatchers("/styles/login.css").permitAll()
+                .antMatchers("/styles/signup.css").permitAll()
+                .antMatchers("/styles/profile.css").permitAll()
+                .antMatchers("/styles/reset.css").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login")
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .logoutSuccessUrl("/index")
+                .deleteCookies("JSESSIONID");
+    }
+
+}
